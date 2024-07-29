b724c18435feb67f39870eb273fbb83842050ff8
JhonataDavid <ferreiradev05@gmail.com>
2024-07-28 22:56:34 -0300

    Adiciona nova class Test.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,5 +3,7 @@ import com.listadepessoas.Pessoa;
 
 public class Test {
     public static void main(String[] args) {
+        Criar pessoas = new Criar();
+        pessoas.mostrarListaDePessoas();
     }
 }
